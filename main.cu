cb48d192dc8e362b1c871daa6450881e43c96492
doohochang <doohochang@gmail.com>
2019-05-05 20:00:51 +0900

    Implement nn model structure

--- main.cu
+++ main.cu
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 #include "hparams.h"
+#include "model.h"
 
 int main(int argc, char** argv) {
     printf("Hello World!\n");
---
+++ model.h
@@ -0,0 +1,21 @@
+#ifndef _model_h_
+#define _model_h_
+
+#include "hparams.h"
+
+typedef struct model {
+    ModelSpec spec;
+
+    /*
+        Weight matrices of hidden layers in model.
+        i th matrix spec:
+            row = number of nodes in i-1 th layer
+            col = number of nodes in i th layer
+    */
+    float **weight_matrices;
+
+    // bias vectors of hidden layers
+    float **biases; 
+} Model; 
+
+#endif
